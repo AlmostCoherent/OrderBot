c52cbb35319ae4d02a6821296fb2cac3a9b6cfd3
Ben Harman <mrb.harman@gmail.com>
2018-10-18 07:39:55 +0100

    Unit test

--- OrderBotTests/Dialogs/DialogFactoryTests.cs
+++ OrderBotTests/Dialogs/DialogFactoryTests.cs
@@ -1,15 +1,42 @@
 ﻿using FluentAssertions;
+using Microsoft.Bot.Builder.Dialogs;
 using Moq;
 using OrderBot.Dialogs.Support;
+using System;
+using System.Threading.Tasks;
 using Xunit;
 
 
 namespace OrderBot.Dialogs.Tests
 {
+    public interface IChatHelper
+    {
+        Task PostAsync(IDialogContext context, string message);
+    }
+    [Serializable]
+    public class ChatHelper : IChatHelper
+    {
+        public async Task PostAsync(IDialogContext context, string message)
+        {
+            await context.PostAsync(message);
+        }
+    }
+
     public class DialogFactoryTests
     {
+        private Mock<IChatHelper> _chat;
+        private RootDialog _dialog;
+        private Mock<IDialogContext> _context;
+
+                public void SetUp()
+        {
+            _chat = new Mock<IChatHelper>();
+            _dialog = new RootDialog();
+            _context = new Mock<IDialogContext>();
+        }
+
         [Fact]
-        public void CreateTest()
+        public async void CreateTest()
         {
             var mockService = new Mock<IDialogFactory>();
             mockService.Setup(m => m.Create("Support"));
